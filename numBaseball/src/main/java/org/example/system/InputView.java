b3115784d730adce3b3ffef4f3448f4d180b6238
Halo <87004079+Halo-oo@users.noreply.github.com>
2024-05-12 22:45:55 +0900

    fix: 사용자 입력숫자 저장 시 범위체크 검증 추가

--- numBaseball/src/main/java/org/example/system/InputView.java
+++ numBaseball/src/main/java/org/example/system/InputView.java
@@ -1,14 +1,19 @@
 package org.example.system;
 
+import org.example.global.util.DataValidation;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class InputView {
     final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    final DataValidation dataValidation = new DataValidation();
 
     /**
      * (기본) 사용자 입력받기
@@ -21,9 +26,15 @@ public class InputView {
      * 입력 받은 3개의 숫자 List 조회
      */
     public List<Integer> getUserInputNumList(String playerNumInput) {
+        List<Integer> playerInputNumList = new ArrayList<>();
 
-        return Arrays.stream(playerNumInput.split(" "))
-                .map(Integer::parseInt)
-                .collect(Collectors.toList());
+        for (String playerNumStr: playerNumInput.split(" ")) {
+            int playerNum  = Integer.parseInt(playerNumStr);
+
+            if (dataValidation.checkNumRange(playerNum)) {
+                playerInputNumList.add(playerNum);
+            }
+        }
+        return playerInputNumList;
     }
 }
