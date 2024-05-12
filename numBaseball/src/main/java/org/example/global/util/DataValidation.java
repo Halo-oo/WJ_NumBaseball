7cab9f9cf1c212f447fe6eadc745c4ee47e3a21a
Halo <87004079+Halo-oo@users.noreply.github.com>
2024-05-12 22:25:21 +0900

    feat: 숫자 범위 체크 검사 구현

--- numBaseball/src/main/java/org/example/global/util/DataValidation.java
+++ numBaseball/src/main/java/org/example/global/util/DataValidation.java
@@ -6,13 +6,25 @@ public class DataValidation {
 
     /**
      * 두 List 내 중복 숫자값 존재 유무 확인
-     * @return 존재 한다면 true ↔️ 존재 하지 않는 다면 false
      */
     public boolean checkDuplicateNum(List<Integer> numList, int randomNum) {
+
         if (numList.contains(randomNum)) {
             return true;
         }
+        return false;
+    }
 
+    /**
+     * 숫자 범위 체크
+     */
+    public boolean checkNumRange(int num) {
+        NumberRange MIN_RANDOM_NUM_RANGE = NumberRange.MIN_RANDOM_NUM_RANGE;
+        NumberRange MAX_RANDOM_NUM_RANGE = NumberRange.MAX_RANDOM_NUM_RANGE;
+
+        if (MIN_RANDOM_NUM_RANGE.getValue() <= num && num <= MAX_RANDOM_NUM_RANGE.getValue()) {
+            return true;
+        }
         return false;
     }
 }
