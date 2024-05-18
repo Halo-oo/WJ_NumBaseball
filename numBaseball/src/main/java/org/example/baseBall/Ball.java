d8753447a7bee4e540fa1005fef8807fe4f6a3b3
Halo <87004079+Halo-oo@users.noreply.github.com>
2024-05-18 21:57:45 +0900

    feat: 야구공(숫자공) class 생성

---
+++ numBaseball/src/main/java/org/example/baseBall/Ball.java
@@ -0,0 +1,22 @@
+package org.example.baseBall;
+
+public class Ball {
+    /* 숫자 (1-9까지의 범위) */
+    int number;
+
+    /* 위치 */
+    int position;
+
+    public Ball(int number, int position) {
+        number = this.number;
+        position = this.position;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+}
